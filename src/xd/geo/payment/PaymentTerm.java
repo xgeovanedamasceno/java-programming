9efaa475f1291674fd7517d5058be7067c9a7a1f
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2021-08-30 12:30:50 -0300

    Read keyboard inputs

---
+++ src/xd/geo/payment/PaymentTerm.java
@@ -0,0 +1,28 @@
+package xd.geo.payment;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+public class PaymentTerm {
+    public static void main(String[] args) {
+
+        var read = new Scanner(System.in);
+
+        System.out.println("Say the day:\n");
+        var day = read.nextInt();
+        System.out.println("Say the month:\n");
+        var month = read.nextInt();
+        System.out.println("Say the year:\n");
+        var year = read.nextInt();
+
+        System.out.println("The date is " + day + " " + month + " " + year);
+
+        Calendar dueDate = new GregorianCalendar();
+
+        dueDate.set(year, month, day);
+        System.out.println(dueDate.getTime());
+
+
+    }
+}
