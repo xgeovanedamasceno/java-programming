5eb9e4a231d504409f5f1c066427e76e3e37533a
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2021-09-01 13:21:54 -0300

    Create Multiple class

---
+++ src/xd/geo/aelum/exer/sintaxe/chapthree/Multiple.java
@@ -0,0 +1,11 @@
+package xd.geo.aelum.exer.sintaxe.chapthree;
+
+public class Multiple {
+    public static void main(String[] args) {
+        int num = 3;
+        for(int i = 1; i <= 100; i++) {
+            int multp = num * i;
+            System.out.println(multp);
+        }
+    }
+}
