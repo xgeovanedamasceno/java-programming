2a68571648886c86e2e75bafcd9c1db43758a66b
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2021-09-01 16:55:36 -0300

    Create Factorial class

--- src/xd/geo/aelum/exer/sintaxe/chapthree/Factorial.java
+++ src/xd/geo/aelum/exer/sintaxe/chapthree/Factorial.java
@@ -1,8 +1,8 @@
-package xd.geo.aelum.exer.sintaxe;
+package xd.geo.aelum.exer.sintaxe.chapthree;
 
 public class Factorial {
     public static void main(String[] args) {
-        long fact = 1;
+        long fact = 1L;
         for(int n = 1; n <= 20; n++) {
             fact = fact * n;
             System.out.println(fact);
