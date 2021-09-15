687515b43871a25829ed979515ff37d9aaccd72c
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2021-09-15 11:06:48 -0300

    Create Employee.class

---
+++ src/xd/geo/aelum/oop/EmployeeTest.java
@@ -0,0 +1,11 @@
+package xd.geo.aelum.oop;
+
+public class EmployeeTest {
+
+    public static void main(String[] args) {
+        Manager man1 = new Manager();
+
+        man1.setSalary(1000);
+        System.out.println(man1.getBonus());
+    }
+}
