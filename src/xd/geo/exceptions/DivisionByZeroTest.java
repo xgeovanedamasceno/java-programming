edac23b80934c0b730dd5be0d808d4d871b8c8d8
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2021-09-16 15:17:01 -0300

    Create DivisionByZeroTest.java

---
+++ src/xd/geo/exceptions/DivisionByZeroTest.java
@@ -0,0 +1,9 @@
+package xd.geo.exceptions;
+
+public class DivisionByZeroTest {
+    public static void main(String[] args) {
+        int i = 4713;
+        i = i / 0;
+        System.out.println("Result: " + i);
+    }
+}
