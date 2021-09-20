b23d0ef92a608f987016d6514d4cc1a9e44348c3
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2021-09-20 09:36:06 -0300

    Create LangTest.java

---
+++ src/xd/geo/lang/LangTest.java
@@ -0,0 +1,11 @@
+package xd.geo.lang;
+
+import java.io.PrintStream;
+
+public class LangTest {
+    public static void main(String[] args) {
+
+        PrintStream exit = System.out;
+        exit.println("Hello World");
+    }
+}
