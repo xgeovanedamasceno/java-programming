9ba2df0319bb8714a111b4483083507a3d88c8f7
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2021-09-20 12:03:01 -0300

    Create StringBuilder reference

--- src/xd/geo/lang/LangTest.java
+++ src/xd/geo/lang/LangTest.java
@@ -8,26 +8,33 @@ public class LangTest {
         PrintStream exit = System.out;
         exit.println("Hello World");
 
-        String im = "   TodayIsMonday 19 ";
-        im = im.replace("19", "20");
-        System.out.println(im);
+        String im = "TodayIsMonday 19";
+//        im = im.replace("19", "20");
+//        System.out.println(im);
+//        System.out.println(im.length());
+//        System.out.println(im.contains("Monday"));
+//        System.out.println(im.contains("monday"));
+//        im = im.trim();
+//        System.out.println(im);
+//        String emptyString = "     ";
+//        System.out.println(emptyString.isEmpty());
+//        System.out.println(emptyString.isBlank());
+//        System.out.println(im.isEmpty());
+//        System.out.println(im.isBlank());
+//
+//        var today = im;
+//        System.out.println(today.length());
+//        StringPlay.loop(today);
+//
+//        StringPlay.inverse(today);
+//
         System.out.println(im.length());
-        System.out.println(im.contains("Monday"));
-        System.out.println(im.contains("monday"));
-        im = im.trim();
-        System.out.println(im);
-        String emptyString = "     ";
-        System.out.println(emptyString.isEmpty());
-        System.out.println(emptyString.isBlank());
-        System.out.println(im.isEmpty());
-        System.out.println(im.isBlank());
-
-        var today = im;
-        System.out.println(today.length());
-        StringPlay.loop(today);
-
-        StringPlay.inverse(today);
-
-
+        String ot = "Tomorrow it will be Tuesday and the forescat is saying that will continues hot";
+        System.out.println("Ot.length: " + ot.length());
+        System.out.println("im.length: " + im.length());
+        System.out.println(ot.length()+im.length());
+        var stsum = im + "b";
+        StringBuilder sb = new StringBuilder(stsum);
+        System.out.println(sb.capacity());
     }
 }
