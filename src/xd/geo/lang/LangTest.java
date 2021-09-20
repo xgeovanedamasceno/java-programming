c454ce521751523153b80a6c7f31030bd6d85a68
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2021-09-20 14:28:35 -0300

    Uncomment LangTest.java

--- src/xd/geo/lang/LangTest.java
+++ src/xd/geo/lang/LangTest.java
@@ -9,25 +9,25 @@ public class LangTest {
         exit.println("Hello World");
 
         String im = "TodayIsMonday 19";
-//        im = im.replace("19", "20");
-//        System.out.println(im);
-//        System.out.println(im.length());
-//        System.out.println(im.contains("Monday"));
-//        System.out.println(im.contains("monday"));
-//        im = im.trim();
-//        System.out.println(im);
-//        String emptyString = "     ";
-//        System.out.println(emptyString.isEmpty());
-//        System.out.println(emptyString.isBlank());
-//        System.out.println(im.isEmpty());
-//        System.out.println(im.isBlank());
-//
-//        var today = im;
-//        System.out.println(today.length());
-//        StringPlay.loop(today);
-//
-//        StringPlay.inverse(today);
-//
+        im = im.replace("19", "20");
+        System.out.println(im);
+        System.out.println(im.length());
+        System.out.println(im.contains("Monday"));
+        System.out.println(im.contains("monday"));
+        im = im.trim();
+        System.out.println(im);
+        String emptyString = "     ";
+        System.out.println(emptyString.isEmpty());
+        System.out.println(emptyString.isBlank());
+        System.out.println(im.isEmpty());
+        System.out.println(im.isBlank());
+
+        var today = im;
+        System.out.println(today.length());
+        StringPlay.loop(today);
+
+        StringPlay.inverse(today);
+
         System.out.println(im.length());
         String ot = "Tomorrow it will be Tuesday and the forescat is saying that will continues hot";
         System.out.println("Ot.length: " + ot.length());
