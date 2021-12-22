dfb2e98ebf09b537866f1fc25226922f21ead109
xgeovanedamasceno <31930510+xgeovanedamasceno@users.noreply.github.com>
2021-12-22 06:21:49 -0300

    Delete src/xd/geo directory

--- src/xd/geo/aelum/oop/InsufficientBalanceException.java
+++
@@ -1,8 +0,0 @@
-package xd.geo.aelum.oop;
-
-public class InsufficientBalanceException extends RuntimeException {
-
-    public InsufficientBalanceException(String message, double value) {
-        super(message + " " + value);
-    }
-}
